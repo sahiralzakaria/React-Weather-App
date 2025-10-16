83d5a2b2360f518e137279bc045a1da5cdbab98d
sahir zakaria <sahir.alzakaria@gmail.com>
2025-10-16 03:32:40 +0300

    initial ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Weather from './components/Weather'
 
 const App = () => {
   return (
-    <div className='app'>App</div>
+    <div className='app'>
+      <Weather/>
+    </div>
   )
 }
 
